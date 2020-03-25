7528ca6fbc3ddc968c5826c389ea10b6c3d3a522
FullMentalPanic <ganliang0122@gmail.com>
2020-03-26 00:55:20 +0800

    udpate jieba dict & check resouce release time and string pattern ratio

--- src/SearchKeyWord.py
+++ src/SearchKeyWord.py
@@ -7,13 +7,15 @@ import requests
 import re
 from bs4 import BeautifulSoup
 import jieba
+from datetime import datetime
+import difflib
 
 try:
     from urllib.parse import quote
 except ImportError:
     from urllib import pathname2url as quote
 
-DEBUG = 0
+DEBUG = 1
 
 class dmhy_search(object):
     domain = "https://share.dmhy.org"
@@ -33,7 +35,7 @@ class dmhy_search(object):
        "User-Agent":"Mozilla/5.0 (Windows NT 10.0.14393; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2950.5 Safari/537.36"
        }
 
-    def __init__(self, keyword= str(), episodes = list, class_num = 0):
+    def __init__(self, keyword= str(), episodes = list, class_num = 0,min_time = datetime(2018, 9, 1, 0, 0), basepattern = ''):
         self.search_list_info = dict() # title , seed, finish, magent
         self.search_list_magent = []
         self.number = episodes
@@ -41,8 +43,11 @@ class dmhy_search(object):
         self.search_class = group[class_num]
         key = quote(keyword)
         self.url = self.search_link + key
+        self.min_time = min_time
+        self.basepattern = basepattern
         for num in self.number:
             self.search_list_info[num] = []
+        jieba.load_userdict('dict.txt') 
 
     def run(self):       
         self.extract_info()
@@ -63,15 +68,20 @@ class dmhy_search(object):
             animate = animate_infos.find_all("tr", class_="")
             for link in animate:
                 tmp = link.find("td", "title")
+                dnd_time = ((link.find("span", style ="display: none;")).text.strip())[0:10]
+                #print (dnd_time)
+                dnd_time=datetime.strptime(dnd_time, '%Y/%m/%d')
+
                 title = (tmp.find("a", target="_blank")).text.strip()
                 Web_class = (link.find("font")).text.strip()
-                if Web_class == self.search_class:
+                if Web_class == self.search_class and dnd_time > self.min_time:
                     tmp = link.find_all("td", nowrap="nowrap")
                     magent = (tmp[0].a).get('href') #magent
                     size = tmp[1].text.strip() #size
                     seed = tmp[2].text.strip() # seed
                     finished = tmp[3].text.strip() # finished
-                    seg_list = jieba.cut(title, cut_all=True, HMM=True)
+                    seg_list = jieba.cut(title, HMM=True)
+                    #print (", ".join(seg_list))
                     for item in seg_list: 
                         try:
                             temp = int(item)
@@ -93,13 +103,31 @@ class dmhy_search(object):
     def sort_hot_resource(self):
         for key in self.search_list_info:
             temp_list = self.search_list_info[key]
+            #print (temp_list)
             if len(temp_list) is not  0:
-                temp_list.sort(key=lambda x: (x[1], x[2]))
-                self.search_list_magent.append(temp_list[-1])
+                if self.basepattern is '':
+                    temp_list.sort(key=lambda x: (x[1], x[2]))
+                    self.search_list_magent.append(temp_list[-1])
+                else:
+                    max_ratio = 0
+                    result = []
+                    for item in temp_list:
+                        ratio = self.string_similar(item[0])
+                        if ratio > max_ratio:
+                            max_ratio = ratio
+                            result = item
+                    self.search_list_magent.append(result) 
+
+
+    def string_similar(self,input_str = str()):
+        return difflib.SequenceMatcher(None, self.basepattern, input_str).quick_ratio()
+
+
+
 
 
 
 
 if DEBUG == 1:
-    test = dmhy_search("海贼王",[905,906,907,908,909,910])
+    test = dmhy_search("刀剑神域 Alicization War of Underworld",[3], basepattern = '[c.c動漫][10月新番][刀劍神域 Alicization War of Underworld][01][BIG5][1080P][MP4][網盤]')
     print (test.run())#海贼王 905 923
